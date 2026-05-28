9e7d5829159eec87d1bc18437e92cef45e8f08ea
Frey Tim <tfr@ti8m.ch>
2026-05-28 18:28:54 +0200

    removed revoked credentials

--- taxme-mock/environment.ts
+++ taxme-mock/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  SOLID_CLIENT_ID: "bc13513c-8a92-47e4-a932-7656220161ce",
-  SOLID_CLIENT_SECRET: "06e876dc-2b49-48d3-9e33-4dfc4ffd079a",
-  SOLID_OIDC_ISSUER: "https://login.inrupt.com",
+  SOLID_CLIENT_ID: "",
+  SOLID_CLIENT_SECRET: "",
+  SOLID_OIDC_ISSUER: "",
   SOLID_RESOURCE_URL: ""
 };
